b8286957b13ace91b37006c32774711dab8539da
Sang Ki Kwon <cranberrygame@yahoo.com>
2015-04-10 00:42:38 +0900

    initial xdk version

---
+++ appcompat/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\cordova\avoidbirdchromecast\plugins\com.cranberrygame.cordova.plugin.androidsupportlibrary\appcompat\bin\AndroidManifest.xml : \
+E:\cordova\avoidbirdchromecast\plugins\com.cranberrygame.cordova.plugin.androidsupportlibrary\appcompat\AndroidManifest.xml \
---
+++ mediarouter/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\cordova\avoidbirdchromecast\plugins\com.cranberrygame.cordova.plugin.androidsupportlibrary\mediarouter\bin\AndroidManifest.xml : \
+E:\cordova\avoidbirdchromecast\plugins\com.cranberrygame.cordova.plugin.androidsupportlibrary\mediarouter\AndroidManifest.xml \
